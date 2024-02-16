110d222e5b7a045ead404b4c37cf6b260b3e0a2e
Maurot11 <mauro.co.uk@hotmail.com>
2024-02-16 16:48:36 +0000

    media queries 2560px & 1024px

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,9 @@ import ResetButton from "./ResetButton"
 export default function Header({setCount, setPerS}) {
     return (
         <header>
+            <a href="https://github.com/MauroT11" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Github" />
+            </a>
             <h1>Cookie Clicker</h1>
             <ResetButton resetCount={setCount} resetPerS={setPerS} />
         </header>
